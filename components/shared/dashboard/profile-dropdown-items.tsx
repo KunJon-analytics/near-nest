7ff660376a72d0e9ff780cbf2a2829cccf748648
KunJon-analytics <johnadelakun1@gmail.com>
2024-03-03 23:20:53 +0100

    fixed broken profile link

--- components/shared/dashboard/profile-dropdown-items.tsx
+++ components/shared/dashboard/profile-dropdown-items.tsx
@@ -24,7 +24,7 @@ const ProfileDropdownItems = ({ isHost }: IProps) => {
   return (
     <div className="flex flex-col pt-2 pb-5">
       <Link
-        href={isHost ? "/dashboard/host" : "/dashboard/users"}
+        href={isHost ? "/dashboard/host" : "/dashboard/user"}
         className="group flex items-center space-x-3 py-2 px-4 tracking-wide outline-none transition-all hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600"
       >
         <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-warning text-white">

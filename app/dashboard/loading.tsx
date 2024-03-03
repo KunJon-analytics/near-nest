f10672b04b4d34e945adc18c81709fbb16e58095
KunJon-analytics <johnadelakun1@gmail.com>
2024-03-03 11:03:36 +0100

    added loading

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { LoadingSkeleton } from "@/components/shared/loading-skeleton";
+
+const LoadingPage = () => {
+  return <LoadingSkeleton />;
+};
+
+export default LoadingPage;
